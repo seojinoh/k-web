7688fbd2a84141573e875003065acc33974f4cde
seojinoh <seojinoh.me@gmail.com>
2024-10-04 15:10:15 +0900

    feat: implement Mock repository to provide CRUD functionality

--- src/main/kotlin/com/example/kweb/repository/mock/MockItemRepository.kt
+++ src/main/kotlin/com/example/kweb/repository/mock/MockItemRepository.kt
@@ -8,89 +8,76 @@ import java.sql.Timestamp
 import java.time.Instant
 
 class MockItemRepository: ItemRepository {
-    override fun readAllItems(): List<ManagedItem> = listOf(
-        ManagedItem(
-            id = 1,
-            createTimestamp = Timestamp.from(Instant.parse("2024-01-01T00:00:00.000Z")),
-            price = 119_000L,
-            count = 1,
-            item = Item(
-                name = "Nike Cortez",
-                description = "275 mm",
-                category = "sneakers"
-            )
-        ),
-        ManagedItem(
-            id = 2,
-            createTimestamp = Timestamp.from(Instant.parse("2024-02-01T00:00:00.000Z")),
-            price = 210_000,
-            count = 1,
-            item = Item(
-                name = "Dr. Martens 1461",
-                description = "265 mm",
-                category = "dress_shoes"
-            )
-        ),
-        ManagedItem(
-            id = 3,
-            createTimestamp = Timestamp.from(Instant.parse("2024-03-01T00:00:00.000Z")),
-            price = 69_000L,
-            count = 1,
-            item = Item(
-                name = "Crocs Classic Clog",
-                description = "275 mm",
-                category = "sandals"
+    companion object {
+        private val managedItemMap: MutableMap<Long, ManagedItem> = mutableMapOf(
+            1L to ManagedItem(
+                id = 1,
+                createTimestamp = Timestamp.from(Instant.parse("2024-01-01T00:00:00.000Z")),
+                price = 119_000,
+                count = 1,
+                item = Item(
+                    name = "Nike Cortez",
+                    description = "275 mm",
+                    category = "sneakers"
+                )
+            ),
+            2L to ManagedItem(
+                id = 2,
+                createTimestamp = Timestamp.from(Instant.parse("2024-02-01T00:00:00.000Z")),
+                price = 210_000,
+                count = 1,
+                item = Item(
+                    name = "Dr. Martens 1461",
+                    description = "265 mm",
+                    category = "dress_shoes"
+                )
+            ),
+            3L to ManagedItem(
+                id = 3,
+                createTimestamp = Timestamp.from(Instant.parse("2024-03-01T00:00:00.000Z")),
+                price = 69_000L,
+                count = 1,
+                item = Item(
+                    name = "Crocs Classic Clog",
+                    description = "275 mm",
+                    category = "sandals"
+                )
             )
         )
-    )
+    }
 
-    override fun readItemWithFilter(filter: Filter): List<ManagedItem> = listOf(
-        ManagedItem(
-            id = 1,
-            createTimestamp = Timestamp.from(Instant.parse("2024-01-01T00:00:00.000Z")),
-            price = 119_000L,
-            count = 1,
-            item = Item(
-                name = "Nike Cortez",
-                description = "275 mm",
-                category = "sneakers"
-            )
-        ),
-        ManagedItem(
-            id = 2,
-            createTimestamp = Timestamp.from(Instant.parse("2024-02-01T00:00:00.000Z")),
-            price = 210_000,
-            count = 1,
-            item = Item(
-                name = "Dr. Martens 1461",
-                description = "265 mm",
-                category = "dress_shoes"
-            )
-        ),
-        ManagedItem(
-            id = 3,
-            createTimestamp = Timestamp.from(Instant.parse("2024-03-01T00:00:00.000Z")),
-            price = 69_000L,
-            count = 1,
-            item = Item(
-                name = "Crocs Classic Clog",
-                description = "275 mm",
-                category = "sandals"
-            )
-        )
-    )
+    override fun readAllItems(): List<ManagedItem> = managedItemMap.values.toList()
+        .sortedByDescending { managedItem ->
+            managedItem.id
+        }
 
-    override fun readItemById(id: Long): ManagedItem = ManagedItem(
-        id = 1,
-        createTimestamp = Timestamp.from(Instant.parse("2024-01-01T00:00:00.000Z")),
-        price = 119_000L,
-        count = 1,
-        item = Item(
-            name = "Nike Cortez",
-            description = "275 mm",
-            category = "sneakers"
-        )
-    )
+    override fun readItemWithFilter(filter: Filter): List<ManagedItem> {
+        val filteredManagedItems = managedItemMap.values.toList()
+            .filter { managedItem ->
+                val search = filter.search ?: ""
+                managedItem.item.name.contains(search)
+                        || managedItem.item.description.contains(search)
+                        || managedItem.item.category.contains(search)
+            }
+            .sortedByDescending { managedItem ->
+                managedItem.id
+            }
+
+        val page = filter.page.toInt()
+        val pageSize = filter.pageSize.toInt()
+        val filteredCount = filteredManagedItems.size
+
+        val fromIndex = (page - 1) * pageSize
+        val toIndex = minOf(fromIndex + pageSize, filteredCount)
+
+        return if (fromIndex < filteredCount) {
+            filteredManagedItems.subList(fromIndex, toIndex)
+        } else {
+            emptyList()
+        }
+    }
+
+    override fun readItemById(id: Long): ManagedItem? = managedItemMap[id]
 
     override fun createItem(
         name: String,
@@ -98,17 +85,23 @@ class MockItemRepository: ItemRepository {
         category: String,
         price: Long?,
         count: Long?
-    ): ManagedItem = ManagedItem(
-        id = 4,
-        createTimestamp = Timestamp.from(Instant.parse("2024-04-01T00:00:00.000Z")),
-        price = 139_000L,
-        count = 1,
-        item = Item(
-            name = "Adidas Superstar",
-            description = "275 mm",
-            category = "sneakers"
-        )
-    )
+    ): ManagedItem {
+        val id = managedItemMap.keys.maxOrNull()?.let { maxId -> maxId + 1 } ?: 1
+        val managedItem = ManagedItem(
+                id,
+                createTimestamp = Timestamp.from(Instant.now()),
+                price,
+                count,
+                item = Item(
+                    name,
+                    description,
+                    category
+                )
+            )
+        managedItemMap[id] = managedItem
+
+        return managedItem
+    }
 
     override fun updateItemById(
         id: Long,
@@ -116,27 +109,29 @@ class MockItemRepository: ItemRepository {
         category: String,
         price: Long?,
         count: Long?
-    ): ManagedItem = ManagedItem(
-        id = 4,
-        createTimestamp = Timestamp.from(Instant.parse("2024-04-01T00:00:00.000Z")),
-        price = 139_000L,
-        count = 1,
-        item = Item(
-            name = "Adidas Superstar",
-            description = "275 mm",
-            category = "sneakers"
-        )
-    )
+    ): ManagedItem? {
+        val oldManagedItem = managedItemMap[id]
 
-    override fun deleteItemById(id: Long): ManagedItem = ManagedItem(
-        id = 4,
-        createTimestamp = Timestamp.from(Instant.parse("2024-04-01T00:00:00.000Z")),
-        price = 139_000L,
-        count = 1,
-        item = Item(
-            name = "Adidas Superstar",
-            description = "275 mm",
-            category = "sneakers"
-        )
-    )
+        return if (oldManagedItem != null) {
+            val newManagedItem = oldManagedItem.copy(
+                price = price,
+                count = count,
+                item = oldManagedItem.item.copy(
+                    description = description,
+                    category = category
+                )
+            )
+            managedItemMap[id] = newManagedItem
+
+            newManagedItem
+        } else {
+            null
+        }
+    }
+
+    override fun deleteItemById(id: Long): ManagedItem? = managedItemMap[id]?.let { managedItem ->
+        managedItemMap.remove(id)
+
+        managedItem
+    }
 }
